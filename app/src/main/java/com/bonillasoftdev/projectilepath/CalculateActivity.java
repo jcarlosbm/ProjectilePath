e7b409c9c13b23b7420922fe46942eb0932aa826
Juan Carlos Bonilla <juanky1704@gmail.com>
2023-05-17 23:04:54 -0400

    Add UI in MainActivity and create a CalculateActivity and a RecordActivity

---
+++ app/src/main/java/com/bonillasoftdev/projectilepath/CalculateActivity.java
@@ -0,0 +1,14 @@
+package com.bonillasoftdev.projectilepath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate);
+    }
+}
--- app/src/main/java/com/bonillasoftdev/projectilepath/MainActivity.java
+++ app/src/main/java/com/bonillasoftdev/projectilepath/MainActivity.java
@@ -3,12 +3,31 @@ package com.bonillasoftdev.projectilepath;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_calculate, btn_record;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_calculate = (Button) findViewById(R.id.btn_calculate);
+        btn_record = (Button) findViewById(R.id.btn_record);
+
+        btn_calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn_record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
